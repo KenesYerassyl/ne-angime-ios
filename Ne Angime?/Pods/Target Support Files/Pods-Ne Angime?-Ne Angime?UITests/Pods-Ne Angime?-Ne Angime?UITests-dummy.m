4221012e237be91ac3d9604a4b3091c8a0d39915
Kenes Yerassyl <Yerassyl@Yeras-Mac.local>
2021-02-13 12:48:16 +0600

    dependencies are integrated

---
+++ Ne Angime?/Pods/Target Support Files/Pods-Ne Angime?-Ne Angime?UITests/Pods-Ne Angime?-Ne Angime?UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Ne_Angime__Ne_Angime_UITests : NSObject
+@end
+@implementation PodsDummy_Pods_Ne_Angime__Ne_Angime_UITests
+@end
---
+++ Ne Angime?/Pods/Target Support Files/Pods-Ne Angime?Tests/Pods-Ne Angime?Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Ne_Angime_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Ne_Angime_Tests
+@end
